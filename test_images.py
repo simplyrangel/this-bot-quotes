169dd3d5d58debe8e3a018c6809a33d7802183ba
John Rangel <rangel.john.j@gmail.com>
2020-11-22 10:16:06 -0600

    Create all quote images at once for inspection.

---
+++ test_images.py
@@ -0,0 +1,32 @@
+"""Create images of all the quotes to see how they appear."""
+import numpy as np
+import pandas as pd
+import helper.tweetwrapper as twrap
+import helper.images as images
+
+# read quotes data:
+df = pd.read_csv("bookquotes.csv")
+df = df.fillna("unknown")
+
+# create images for all the quotes:
+for quote_id in df.index:
+    auth = df.loc[quote_id,"author"]
+    book = df.loc[quote_id,"title"]
+    quote = df.loc[quote_id, "quote"]
+
+    # add quotes around the quote text and book text:
+    quote = "'%s'" %quote
+    if book!="unknown":
+        book="'%s'" %book
+
+    # create tweet image:
+    tweet_image_text = images.quote2image(
+        auth,
+        book,
+        quote,
+        outfi="bin/sample_image_%d.png" %quote_id)
+    
+
+
+
+
